68a0fe2de951c1566d6b0796c3b59aff043ccf64
Luis Correa <helios@polispay.org>
2021-02-25 12:21:47 -0600

    Update auth.go

--- auth/auth.go
+++ auth/auth.go
@@ -49,16 +49,16 @@ func Middleware(db *gorm.DB) func(http.Handler) http.Handler {
 				return
 			}
 
-			key := APIKeyInfo{
+			/*key := APIKeyInfo{
 				APIKey:    keyInfo.Key,
 				ClientID:  int64(keyInfo.Client.ID),
 				IsAdmin:   keyInfo.IsAdmin,
 				IsPremium: keyInfo.Client.IsPremium,
 				IsDev:     keyInfo.IsDev,
-			}
+			}*/ //TODO add missing info to the model
 
 			// put it in context
-			ctx := context.WithValue(r.Context(), userCtxKey, key)
+			ctx := context.WithValue(r.Context(), userCtxKey, keyInfo.Client)
 
 			// and call the next with our new context
 			r = r.WithContext(ctx)
@@ -68,8 +68,8 @@ func Middleware(db *gorm.DB) func(http.Handler) http.Handler {
 }
 
 // ForContext finds the user from the context. REQUIRES Middleware to have run.
-func ForContext(ctx context.Context) *APIKeyInfo {
-	raw, _ := ctx.Value(userCtxKey).(*APIKeyInfo)
+func ForContext(ctx context.Context) *model.Client {
+	raw, _ := ctx.Value(userCtxKey).(*model.Client)
 	return raw
 }
 
